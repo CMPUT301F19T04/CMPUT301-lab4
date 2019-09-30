ce550e90af7be4fb5e639baa0039f5810a096937
Mark Thomas <MarkTee@users.noreply.github.com>
2019-09-30 17:44:59 -0600

    Change Shape color to green

--- Lab4Exercise/app/src/main/java/com/example/lab4exercise/Shape.java
+++ Lab4Exercise/app/src/main/java/com/example/lab4exercise/Shape.java
@@ -3,6 +3,9 @@ package com.example.lab4exercise;
 public abstract class Shape {
     private int x;
     private int y;
+
+    private String color = "green";
+    
     Shape(int x, int y){
         this.x = x;
         this.y = y;
