4c38a2743e8e26bf0eeeefa268b616f0857dcead
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-30 17:56:36 -0600

    change Shape colour

--- Lab4Exercise/app/src/main/java/com/example/lab4exercise/Shape.java
+++ Lab4Exercise/app/src/main/java/com/example/lab4exercise/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    private String color = "Blue";
+    private String color = "#008080";
 
     Shape(int x, int y){
         this.x = x;
